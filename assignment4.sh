a5bc64900b362f8adc8d8c0666b796d183452d0c
Amr M. Kayid <amrkayid2027@gmail.com>
2018-10-30 12:04:32 +0200

    Add go

--- assignment4.sh
+++ assignment4.sh
@@ -4,6 +4,17 @@ print() {
   printf "\n%b\n" "$1"
 }
 
+print "Install go & govendor"
+apk add --no-cache --virtual .build-deps bash gcc musl-dev openssl go
+wget -O go.tgz https://dl.google.com/go/go1.10.3.src.tar.gz
+tar -C /usr/local -xzf go.tgz
+cd /usr/local/go/src/
+./make.bash
+export PATH="/usr/local/go/bin:$PATH" 
+export GOPATH=/opt/go/
+export PATH=$PATH:$GOPATH/bin
+apk del .build-deps
+
 print "Make sure it is installed successfully:"
 go version
 
