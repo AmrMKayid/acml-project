ba72d2c44575c9a3afe7d195096037b0366c4150
Amr M. Kayid <amrkayid2027@gmail.com>
2018-10-30 12:03:06 +0200

    Update assignment4.sh

--- assignment4.sh
+++ assignment4.sh
@@ -4,10 +4,18 @@ print() {
   printf "\n%b\n" "$1"
 }
 
+print "Make sure it is installed successfully:"
+go version
+
+print "Install govendor:"
+go get -u github.com/kardianos/govendor
+
 print "Clone repository"
 git clone https://github.com/ramin0/myapp
 
 cd myapp
+
+ls
 
 rm docker-compose.yml
 rm main.go
@@ -15,12 +23,6 @@ rm main.go
 curl -OL https://raw.githubusercontent.com/AmrMKayid/acml-project/assignments/4/docker-compose.yml
 curl -OL https://raw.githubusercontent.com/AmrMKayid/acml-project/assignments/4/main_withredis.go
 mv main_withredis.go main.go
-
-print "Make sure it is installed successfully:"
-go version
-
-print "Install govendor:"
-go get -u github.com/kardianos/govendor
 
 govendor fetch github.com/go-redis/redis
 govendor fetch github.com/vmihailenco/msgpack
