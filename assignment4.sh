bad2b2aee75d65452b30566054919a93164fb0ea
Amr M. Kayid <amrkayid2027@gmail.com>
2018-10-30 12:01:02 +0200

    Add govendor

--- assignment4.sh
+++ assignment4.sh
@@ -16,6 +16,12 @@ curl -OL https://raw.githubusercontent.com/AmrMKayid/acml-project/assignments/4/
 curl -OL https://raw.githubusercontent.com/AmrMKayid/acml-project/assignments/4/main_withredis.go
 mv main_withredis.go main.go
 
+print "Make sure it is installed successfully:"
+go version
+
+print "Install govendor:"
+go get -u github.com/kardianos/govendor
+
 govendor fetch github.com/go-redis/redis
 govendor fetch github.com/vmihailenco/msgpack
 govendor fetch github.com/go-redis/cache
