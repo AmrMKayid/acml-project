b21f61f6dd7d32484d1636bb7bf0b98998114600
Amr M. Kayid <amrkayid2027@gmail.com>
2018-11-03 21:56:32 +0200

    Update assignment4.sh

--- assignment4.sh
+++ assignment4.sh
@@ -28,6 +28,8 @@ cd myapp
 
 ls
 
+sleep 7
+
 rm docker-compose.yml
 rm main.go
 
@@ -35,10 +37,14 @@ curl -OL https://raw.githubusercontent.com/AmrMKayid/acml-project/assignments/4/
 curl -OL https://raw.githubusercontent.com/AmrMKayid/acml-project/assignments/4/main_with_redis.go
 mv main_with_redis.go main.go
 
+sleep 7
+
 govendor fetch github.com/go-redis/redis
 govendor add +e
 
 print "synchronize external packages using govendor"
 govendor sync
+
+sleep 7
 
 docker-compose up
